e7503348d4eda94418b165db40102c65b2853587
Shab <kydshab@gmail.com>
2023-08-13 18:18:05 -0400

    added addition and substraction

--- calc/calc.py
+++ calc/calc.py
@@ -13,7 +13,7 @@ layout = [
 
 # Create the Window
 window = sg.Window('Calculator', layout)
-line = [''] #init calc line
+line = ['0'] #init calc line
 
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
@@ -21,6 +21,37 @@ while True:
     if event == sg.WIN_CLOSED: break #close when close
 
     if event == 'C': line = ['0'] #clear button
+
+    elif event == '=':
+        while len(line) != 1:
+            if line[1] == '+':
+                eq = float(line[0]) + float(line[2])
+                line[1] = str(eq)
+                line.pop(2)
+                line.pop(0)
+            if line[1] == '-':
+                eq = float(line[0]) - float(line[2])
+                line[1] = str(eq)
+                line.pop(2)
+                line.pop(0)
+        # for i in range(1,len(line)-1):
+        #     # ansline = line
+        #     print('AAA')
+        #     # ind = line.index(i)
+        #     if line[i] == '+':
+        #         eq = float(line[i-1]) + float(line[i+1])
+        #         line[i] = str(eq)
+        #         line.pop(i+1)
+        #         line.pop(i-1)
+        #     print(line)
+        #     # if i == '-':
+        #     #     eq = float(line[ind-1]) - float(line[ind+1])
+        #     #     line[ind] = str(eq)
+        #         # line.pop(ind+1)
+        #         # line.pop(ind-1)
+        # print(line)
+        # # line = ['0'] #SOME OPERATIONS
+
     else:
         try:
             int(event)                                          #try to make the clicked button be integer
@@ -33,17 +64,17 @@ while True:
             elif event == '.' and '.' in line[-1]: pass         #if it exists, pass
             
             else: 
-                if line[-1] == '':
-                    try:
-                        line.pop(-2)
-                        line.pop(-1)
-                    except IndexError: pass
-                line.extend([event, ''])
+                # if line[-1] == '':              #if there is already some operator, then there is ''
+                #     try:                        #this part removes that sign and a '' next to it
+                #         line.pop(-2)
+                #         line.pop(-1)
+                #     except IndexError: pass
+                line.extend([event, '0'])
 
-    values['line'] = ''.join(line)
-    window['line'].update(values['line'])
+    values['line'] = ''.join(line)              #sync line list with the line on the UI
+    window['line'].update(values['line'])       #
 
-    print(values)
+    # print(values)
     print(line)
 
 window.close()
