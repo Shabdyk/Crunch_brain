3d06f10f6b51d4537b323b32de1035c54ff6afbf
Shab <kydshab@gmail.com>
2023-08-15 18:03:26 -0400

    plus_minus & mult_div

--- calc/src.py
+++ calc/src.py
@@ -1,17 +1,53 @@
-def plus_minus(li):
+def plus_minus(li:list):
 
     for i in range(len(li)):
-        print(li[i])
         try:
             li[i] = float(li[i])
         except ValueError:
             if li[i] == '-' or li[i] == '+': 
                 li[i+1] = li[i] + li[i+1]
                 li[i] = ''
-    while '' in li:
-        li.remove('')
+    while '' in li: li.remove('')
     return li
 
-tst = ['3','+','4','-','1','*','2','*','3','-','0']
+def mult_div(li:list):
+    while ('*' in li) or ('/' in li):
+        i = 0
+        j = 0
+        if '*' in li:
+            i = li.index('*')
+        if '/' in li:
+            j = li.index('/')
 
-print(plus_minus(tst))
+        print(str(i) + 'i__j' + str(j))
+
+        if (i < j and i != 0) or j == 0:
+            product = li[i-1] * li[i+1]
+            li[i-1:i+2] = [product]
+            # print('prod')
+        elif (i > j and j != 0) or i == 0:
+            ratio = li[j-1] / li[j+1]
+            li[j-1:j+2] = [ratio]
+            # print('ratio')
+        else:
+            print('Error i = j')
+            break
+
+        print(li)
+
+    if ('/' or '*') in li:
+        print('PANIC')
+
+    return li
+
+
+
+# tst = ['3','+','4','-','1','*','2','*','3','/','2','*', '2', '-', '4', '/', '5']
+
+# plmi = [3.0, 4.0, -1.0, '/', 2.0,  '*', 2.0, -5.0, '/', 5.0, '*' , 5.0, -1.0, '*', 2.0, -4.0, '/', 5.0]
+# # plmi = [3.0, 4.0, -1.0, '*', 2.0, -4.0, '/', 5.0]
+
+# # print(plus_minus(tst))
+
+# # ready = plus_minus(tst)
+# print(mult_div(plmi))
